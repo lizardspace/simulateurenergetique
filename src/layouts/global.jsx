9eedd87c0a148f1ce720df1b176a53a0594f01e7
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-25 21:49:52 +0000

    ggggggggggggggggggggggggg

--- src/layouts/global.jsx
+++ src/layouts/global.jsx
@@ -7,7 +7,14 @@ import Footer from "@/components/footer";
 const Layout = ({ children }) => (
   <>
     <Header />
-    <Box as="main" px={[4, 6, 10, 14, 20]} maxW={MAX_WIDTH} mx="auto">
+    <Box
+      as="main"
+      px={[4, 6, 10, 14, 20]}
+      maxW={MAX_WIDTH}
+      mx="auto"
+      textAlign="center" // Align center
+      my="10px" // Add margin/padding of 5px
+    >
       {children}
     </Box>
     <Footer />
