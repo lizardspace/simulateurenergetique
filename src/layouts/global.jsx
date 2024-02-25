57cc9625d87d21babb1e85ed062e2bbaf732931a
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-25 22:03:33 +0000

    gdfgdffgfff

--- src/layouts/global.jsx
+++ src/layouts/global.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 import { MAX_WIDTH } from "config";
 import Header from "@/components/header";
@@ -7,16 +7,17 @@ import Footer from "@/components/footer";
 const Layout = ({ children }) => (
   <>
     <Header />
-    <Box
-      as="main"
-      px={[4, 6, 10, 14, 20]}
-      maxW={MAX_WIDTH}
-      mx="auto"
-      textAlign="center" // Align center
-      my="10px" // Add margin/padding of 5px
-    >
-      {children}
-    </Box>
+    <Flex justifyContent="center"> {/* Center horizontally */}
+      <Box
+        as="main"
+        px={[4, 6, 10, 14, 20]}
+        maxW={MAX_WIDTH}
+        mx="auto"
+        my="10px" // Add margin/padding
+      >
+        {children}
+      </Box>
+    </Flex>
     <Footer />
   </>
 );
